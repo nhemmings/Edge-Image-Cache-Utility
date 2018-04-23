6ab9dafd9208e6693666f7fab66b629c05be5e0e
Brandon Niewoehner <bniewo@gmail.com>
2018-04-23 11:20:59 -0500

    Add project files.

---
+++ ImageCacheUtility/Actions.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ImageCacheUtility
+{
+    class Actions
+    {
+        private string[] fullPath, oldFilesFullPath;
+        private string cachePath;
+        private bool returnFullPath = false;
+        private DateTime oldDate;
+        private string[] oldFilesDate;
+
+        
+        public dynamic[] FindEmptyFiles()
+        {
+            var di = new DirectoryInfo(cachePath); //create instance of directory information based on cachepath
+            FileInfo[] zeroSizeFiles = di.GetFiles("*.*", SearchOption.AllDirectories).Where(fi => fi.Length == 0).ToArray();// create a file system array and add files that are 0kbs to it
+            fullPath = zeroSizeFiles.Select(file => file.FullName).ToArray(); //add the 0kb file paths to string array
+            if (returnFullPath) //toggle for full path or file name
+            {
+                return fullPath; //return full path
+            }
+            else
+            {
+                return zeroSizeFiles; // return file name
+            }
+
+        }
+
+
+        public void FixEmptyFiles()
+        {
+            //They must provide a path and find the empty files before deleting them
+            if (fullPath is null || fullPath[0] == "")
+            {
+                MessageBox.Show("Please Find Empty Files First","Find Must Be Run");
+            }
+            else
+            { 
+                //passes the full path of each item in the 0kb full path array to the delete command
+                for (int i = 0; i < fullPath.Length; i++)
+                {
+                    File.Delete(fullPath[i]);
+                }
+                MessageBox.Show("Empty Files Deleted", "Delete Empty Files Complete");
+            }
+        }
+
+        public void SetCachePath(string path)
+        {
+            cachePath = path;
+            
+        }
+
+        /*public void Debug_FullPath() //Debug method 
+        {
+            for (int i = 0; i < fullPath.Length; i++)
+            {
+                Console.WriteLine(fullPath[i]);
+            }
+        }*/
+
+        public void SetReturnFullPathToggle() //Method to set toggle of returning full file path or file name 0kb files
+        {
+            if (returnFullPath)
+            {
+                returnFullPath = false;
+            }
+            else
+            {
+                returnFullPath = true;
+            }
+        }
+
+        public void FindOldFiles()
+        {
+            var di = new DirectoryInfo(cachePath); //create instance of directory information based on cachepath
+            FileInfo[] filesCreatedOnOrBeforeDate = di.GetFiles("*.*", SearchOption.AllDirectories).Where(fi => fi.LastWriteTime.Date <= oldDate).ToArray(); //create a file system array and add files with a modified date of the delete date or older 
+            oldFilesFullPath = filesCreatedOnOrBeforeDate.Select(file => file.FullName).ToArray();//add the old file paths to string array
+
+            oldFilesDate = filesCreatedOnOrBeforeDate.Select(file => file.LastWriteTime.ToShortDateString()).ToArray(); //add the last modified date to a string array
+          
+        }
+
+        public void SetDate(DateTime date)
+        {
+            oldDate = date;
+            //Console.WriteLine("old date" + oldDate);
+        }
+
+
+        public string[] ReturnOldFilesFullPath()
+        {
+            return oldFilesFullPath;
+        }
+
+        public string[] ReturnOldFilesModifyDate()
+        {
+            return oldFilesDate;
+        }
+        
+        public void DeleteOldFiles()
+        {
+            //they must find the old files before we delete them
+            if (oldFilesFullPath is null || oldFilesFullPath[0] == "")
+            {
+                MessageBox.Show("Please Find Old Files First", "Find Must Be Run");
+            }
+            else
+            {
+                //passes the full path of each item in the old files full path  array to the delete command
+                for (int i = 0; i < oldFilesFullPath.Length; i++)
+                {
+                    File.Delete(oldFilesFullPath[i]);
+                }
+                MessageBox.Show("Old Files Deleted", "Delete Old Files Complete");
+            }
+        }
+
+
+
+
+    }
+}
---
+++ ImageCacheUtility/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageCacheUtility
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Actions action = new Actions(); //initialize action class
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Find_Click(object sender, RoutedEventArgs e)
+        {
+            //they must have a cache path or it will not run
+            if ((ImageCachePathBox.Text != null) && (ImageCachePathBox.Text != ""))
+            {
+
+                Results.Items.Clear();  //clear list
+                action.SetCachePath(ImageCachePathBox.Text);    //seth cache path with path provided
+                action.FindEmptyFiles();    
+                //action.Debug_FullPath();
+                
+                //add the returned empty files (names or full path) to the list element 
+                for(int i =0; i < action.FindEmptyFiles().Length; i++)
+                {
+                    Results.Items.Add(action.FindEmptyFiles()[i]);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please insert a cache path.", "Cache Path Empty");
+            }
+        }
+
+        private void Fix_Click(object sender, RoutedEventArgs e)
+        {
+            //they must have a cache path or it will not run
+            if ((ImageCachePathBox.Text != null) && (ImageCachePathBox.Text != ""))
+            {
+                action.FixEmptyFiles();
+                Results.Items.Clear(); //clear the list after deleting them, makes it look like the app actually did something.
+            }
+            else
+            {
+                MessageBox.Show("Please insert a cache path.", "Cache Path Empty");
+            }
+        }
+
+        private void CheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            action.SetReturnFullPathToggle();
+        }
+
+        private void Find_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            //they must have a cache path or it will not run
+            if ((ImageCachePathBox_Delete.Text != null) && (ImageCachePathBox_Delete.Text != ""))
+            {
+
+                Results_Delete.Items.Clear(); //clear list
+                //Console.WriteLine("Cleared");
+                action.SetCachePath(ImageCachePathBox_Delete.Text); //set path with path provided in Delete Old Items tab
+               // Console.WriteLine(DesiredRemovalDate.DisplayDate);
+                //Console.WriteLine(DesiredRemovalDate.DisplayDateStart);
+
+                //check for date in past or it will not run todays date will delete entire cache probably not what they want
+                if (DesiredRemovalDate.DisplayDate.Date  >= System.DateTime.Now.Date)
+                {
+                    MessageBox.Show("The delete date is todays date or a future date. Please select a date in the past.", "Removal Date Is Not In Past");
+                }
+                else
+                {
+                    //Console.WriteLine("set date");
+                    action.SetDate(DesiredRemovalDate.DisplayDate); //set delete date based off of date picker
+                    action.FindOldFiles();
+                   // Console.WriteLine("Generate List");
+
+                    //add old files full paths to the list along with last modified date
+                    for (int i = 0; i < action.ReturnOldFilesFullPath().Length; i++)
+                    {
+                        Results_Delete.Items.Add(action.ReturnOldFilesFullPath()[i] + "  ||  Last Modified Date: " + action.ReturnOldFilesModifyDate()[i]);
+                    }
+                    //Console.WriteLine("List Complete");
+                    CountValue.Content = action.ReturnOldFilesFullPath().Length;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please insert a cache path.", "Cache Path Empty");
+            }
+        }
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //date picker listener listens for the date picker to be changed and updates the display date to match
+            var picker = sender as DatePicker;
+            DateTime? date = picker.SelectedDate;
+            DesiredRemovalDate.DisplayDate = date.Value;
+            
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            action.DeleteOldFiles(); //delete old files
+            Results_Delete.Items.Clear(); //clear the list after deleting them, makes it look like the app actually did something.
+        }
+    }
+}
